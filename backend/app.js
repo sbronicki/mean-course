34b4af171ab04ccfcacb342095a7e9ae9d517fa2
sbronicki <sbronicki@gmail.com>
2021-01-03 23:50:26 -0500

    edit post functionality

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
 	next();
 });
 
@@ -40,6 +40,17 @@ app.post('/api/posts', (req, res, next) => {
 	});
 });
 
+app.put('/api/posts/:id', (req, res, next) => {
+	const post = new Post({
+		_id: req.body.id,
+		title: req.body.title,
+		content: req.body.content
+	});
+	Post.updateOne({ _id: req.params.id }, post).then((result) => {
+		res.status(200).json({ message: 'Update Successfull' });
+	});
+});
+
 app.get('/api/posts', (req, res, next) => {
 	Post.find().then((documents) => {
 		res.status(200).json({
@@ -49,9 +60,18 @@ app.get('/api/posts', (req, res, next) => {
 	});
 });
 
+app.get('/api/posts/:id', (req, res, next) => {
+	Post.findById(req.params.id).then((post) => {
+		if (post) {
+			res.status(200).json(post);
+		} else {
+			res.status(404).json({ message: 'Post not found' });
+		}
+	});
+});
+
 app.delete('/api/posts/:id', (req, res, next) => {
 	Post.deleteOne({ _id: req.params.id }).then((result) => {
-		console.log(result);
 		res.status(200).json({ message: 'Post deleted!' });
 	});
 });
