0f84b084bd9354aa0c41a353cf13f0e845ead0f2
sbronicki <sbronicki@gmail.com>
2021-01-04 20:08:52 -0500

    image upload previews

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -16,6 +16,7 @@ export class PostCreateComponent implements OnInit {
 	public post: Post;
 	isLoading = false;
 	form: FormGroup;
+	imagePreview: string;
 	private mode = 'create';
 	private postId: string;
 
@@ -24,7 +25,8 @@ export class PostCreateComponent implements OnInit {
 	ngOnInit() {
 		this.form = new FormGroup({
 			title: new FormControl(null, { validators: [ Validators.required ] }),
-			content: new FormControl(null, { validators: [ Validators.required ] })
+			content: new FormControl(null, { validators: [ Validators.required ] }),
+			image: new FormControl(null, { validators: [ Validators.required ] })
 		});
 		this.route.paramMap.subscribe((paramMap: ParamMap) => {
 			if (paramMap.has('postId')) {
@@ -48,6 +50,19 @@ export class PostCreateComponent implements OnInit {
 				this.postId = null;
 			}
 		});
+	}
+
+	onImageSelect(event: Event) {
+		const file = (event.target as HTMLInputElement).files[0];
+		this.form.patchValue({
+			image: file
+		});
+		this.form.get('image').updateValueAndValidity();
+		const reader = new FileReader();
+		reader.onload = () => {
+			this.imagePreview = reader.result as string;
+		};
+		reader.readAsDataURL(file);
 	}
 
 	onSavePost() {
